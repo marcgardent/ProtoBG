4b377762acf105c8ff65fff3e80f74dd34053fb6
marcgardent <marcgardent@yahoo.fr>
2020-09-27 16:58:05 +0200

    update the doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -201,115 +201,69 @@ export class AppComponent implements OnInit {
   }
 
   public content: string = `
-# example for PAO
+📐myTemplate:
+  tags: 📐nunjucks
+  📐extension: svg
+  📐definition: |
+    <svg xmlns="http://www.w3.org/2000/svg" 
+    width="{{ width }}"
+    height="{{ height }}" viewBox="0 0 {{ width }} {{ height }}" >
+     <style>
+        /* <![CDATA[ */
+        @font-face {
+            font-family: "game-icons";
+            src: url('{{ './assets/game-icons.woff' | includeAsDataUri }}') format('woff');
+        }
+        /* ]]> */
+      </style>
+      <g id="bleedLayer" transform="translate({{bleedbox.x}}, {{bleedbox.y}})">
+        <rect fill="black" id="bleedbox" x="0" y="0" width="{{bleedbox.width}}" height="{{bleedbox.height}}"/>
+      </g>
+      <g id="trimLayer" transform="translate({{trimbox.x}}, {{trimbox.y}})">
+        <rect id="trimbox" x="0" y="0" width="{{trimbox.width}}" height="{{trimbox.height}}"
+            fill="black" rx="{{trimbox.corners}}" ry="{{trimbox.corners}}"/>
+      </g>
+      <g id="artLayer" transform="translate({{artbox.x}}, {{artbox.y}})">
+        <rect id="artbox" x="0" y="0" width="{{artbox.width}}" height="{{artbox.height}}"
+        rx="{{trimbox.corners}}" ry="{{trimbox.corners}}" fill="lightgray" />
+        <text style="font-family: game-icons;font-size:15" x="{{artbox.width/2}}" y="{{artbox.height/2}}" text-anchor="middle">
+            {{ 'icon' | fromModel }}
+        </text>
+        <text style="font: small-caps bold 8px sans-serif" x="{{artbox.width/2}}" y="{{artbox.height/2+12}}" text-anchor="middle" fill="black">
+            {{ 'title' | fromModel }}
+        </text>
+      </g>
+    </svg>
 
-## My domain
+🏠myCard:
+  title: my emoji
 
-🧰goods:
-  tags: 🏢building
+myGlyph:
+    title: my game icon
 
-🏭factory:
-    title: the factory
-    description: amazing factory's description
-    tags: 🏢building
-    📈produce: 1🧰goods
-    📉consume: 10🧱raw
-    ⚒️build: 100🧱raw
+🖨️myPrinting:
+  tags: 🖨️printing
+  📑foreach:
+    - { 📑is: 🖼️myCollection}
+  🖨️mode: 🚀production
+  📏margins: 0📏mm
+  📏density: 300📏dpi
+  
+🖼️myCollection:
+  tags: 🖼️collection
+  ⏹layout: ⏹myLayout
+  📐template: 📐myTemplate
+  📐parameters: {}
+  📑foreach:
+    - { 📑is: 🏠myCard, 🖨️copies: 1 }
+    - { 📑is: myGlyph, 🖨️copies: 3}
 
-## printing
-
-📘myDeck:
-    tags: 🖼️artworks 
-    📑foreach:
-        - { 📑is: 🏭factory, 🖨️copies: 10}
-        - { 📑is: 🧰goods, 🖨️copies: 1}
-    ⏹layout: ⏹myLayout
-    📐template: 📐debugTemplate
-    📐parameters:
-      ⬛left: 📉consume
-      ⬛right: 📈produce
-      ⬛bottom: ⚒️build
-
-⏹myLayout:
-  description: accordingly the https://printeurope.fr specification
+⏹myLayout: 
   tags: ⏹layout
   📄format: 🃏poker 
   🔄orientation: 🔄portrait
   📏paddings: 4📏mm
-  📏bleeds: 2📏mm
+  📏bleeds: 0📏mm
   📏corners: 4📏mm
-
-🖨️myPrinting: 
-  tags: 🖨️printing
-  📑foreach: { 📑is: 📘myDeck }
-  🖨️mode: 🛑review
-  📏density: 300📏dpi
-  📏margins: 10📏mm  
-  
-🖨️myAssembly:
-  tags: 🖨️assembly
-  📑foreach: { 📑is: 📘myDeck }
-  🖨️mode: 🚀production
-  📄format: 📄A4
-  ➕marks: ➕lines
-  🔄orientation: 🔄landscape
-  📏margins: 10📏mm 
-  📏gutters: 5📏mm 
-  📏density: 300📏dpi  
-
-📐debugTemplate:
-  tags: 📐nunjucks 
-  📐definition: |
-    <svg xmlns="http://www.w3.org/2000/svg" 
-    width="{{ width }}"
-    height="{{ height }}" viewBox="0 0 {{ width }} {{ height }}">
-      <g id="bleedLayer" transform="translate({{bleedbox.x}}, {{bleedbox.y}})">
-        <rect id="bleedbox" x="0" y="0" width="{{bleedbox.width}}" height="{{bleedbox.height}}" fill="yellow"/>
-      </g>
-      <g id="trimLayer" transform="translate({{trimbox.x}}, {{trimbox.y}})">
-        <rect id="trimbox" x="0" y="0" width="{{trimbox.width}}" height="{{trimbox.height}}" stroke-width="0.1"
-            fill="lightgreen" rx="{{trimbox.corners}}" ry="{{trimbox.corners}}" stroke="red"/>
-        <text text-anchor="left" alignment-baseline="hanging" x="{{trimbox.corners}}" y="0" font-size="4" fill="red">
-            trimbox
-        </text>
-      </g>
-      <g id="artLayer" transform="translate({{artbox.x}}, {{artbox.y}})">
-        <rect id="artbox" x="0" y="0" width="{{artbox.width}}" height="{{artbox.height}}" stroke-width="1" fill="green" />
-        <text style="font-family: sans-serif;font-size:10" text-anchor="middle" x="{{artbox.width/2}}" y="{{artbox.height/2}}" fill="darkgreen">
-            artbox
-        </text>
-      </g>
-    </svg>
-
-📐myCardTemplate:
-  tags: 📐nunjucks 
-  📐definition: |
-    {% set paddings = '📏paddings' | fromParameters | millimeter  %}
-    {% set corners = '📏corners' | fromParameters | millimeter %}
-
-    {% set contentCorners = corners /2  %}
-    {% set contentWidth = width - (paddings*2)  %}
-    {% set contentHeight = height - (paddings*2)  %}
-    {% set contentHalfWidth = contentWidth/2  %}
-    {% set contentHalfHeight = contentWidth/2  %}
-    
-    {% set lineHeight = 10  %}
-    {% set fontSize = lineHeight-3  %}
-    {% set title = 'name' | fromModel %}
-    {% set icon = 'icon' | fromModel  %}
-    
-    <svg xmlns="http://www.w3.org/2000/svg" 
-    width="{{ width }}"
-    height="{{ height }}" viewBox="0 0 {{ width }} {{ height }}">
-      <rect x="0" y="0" width="{{width}}" height="{{height}}" rx="4" ry="4" fill="gray"/>
-      <g id="content" class="debug" transform="translate({{paddings}}, {{paddings}})">
-        <rect x="0" y="0"  width="{{contentWidth}}" height="{{contentHeight}}" rx="{{contentCorners}}" ry="{{contentCorners}}" fill="red"
-    stroke="green" stroke-width="0"/>
-        <rect id="title" class="debug" width="{{contentWidth}}" height="{{lineHeight}}" rx="{{contentCorners}}" ry="{{contentCorners}}" fill="green"/>
-        <text text-anchor="middle" x="{{contentHalfWidth}}" y="{{fontSize}}" font-size="{{fontSize}}">
-            {{icon}}{{title}}
-        </text>
-      </g>
-    </svg>
 `
 }
